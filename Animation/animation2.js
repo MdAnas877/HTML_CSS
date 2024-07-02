fc930674fda0773a2420b4a90285d3e319a9313c
MdAnas877 <mohammedanas8787@gmail.com>
2024-07-02 13:00:29 +0530

    task completed

---
+++ Animation/animation2.js
@@ -0,0 +1,27 @@
+function handle(){
+    let box1 = document.getElementById('child1');
+    let box2 = document.getElementById('child2');
+    let box3 = document.getElementById('child3');
+    let box4 = document.getElementById('child4');
+
+    console.log(box1);
+
+    let move1 = 0;
+
+    setInterval(animation,10)
+
+    function animation(){
+        
+        if(move1 < 450){
+            console.log("Anas");
+            move1++;
+            box1.style.top = move1 + 'px';
+            box2.style.left = move1 + 'px';
+            box3.style.bottom = move1 + 'px';
+            box4.style.right = move1 + 'px';
+
+        }
+    }
+
+   
+}
